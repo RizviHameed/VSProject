b3908c471f736ef167fcc709695f8b4667428a80
Rizvi Hameed <rizvihm@outlook.com>
2016-03-13 18:59:59 -0400

    New Class

---
+++ GitTest01/NewClass.vb
@@ -0,0 +1,13 @@
+﻿Public Class NewClass
+    Private newPropertyValue As String
+    Public Property NewProperty() As String
+        Get
+            Return newPropertyValue
+        End Get
+        Set(ByVal value As String)
+            newPropertyValue = value
+        End Set
+    End Property
+
+
+End Class
